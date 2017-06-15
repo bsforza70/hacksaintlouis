1ab216881d02f28da1929e4ec4d9635b0a9ffa72
Bob Sforza <rpsforza@gmail.com>
2017-06-15 02:53:38 -0500

    testing slack-git intengration

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
+// Probably shouldn't be doing this ^^^ but okay
 
 session_start();
 
@@ -19,8 +20,7 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/sql-auth.json')) {
 	$db_name = $creds["name"];
 }
 
-function getDB()
-{
+function getDB() {
 	global $db_username, $db_password, $db_server, $db_name;
 
 	if (isset($mysqli) && $mysqli instanceof mysqli) {
