c305537e3bf576fdd4f4cb456b197725ceac7cc8
Michel Ge <strengthofthepen@users.noreply.github.com>
2017-06-20 00:39:25 -0500

    better button

--- index.php
+++ index.php
@@ -68,33 +68,55 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script>
-			$(document).ready(function(){
-				var formSubmitted = function(success) {
-					if (success) {
-						$("#subscribe button").css("background-color", "#00CD00");
-						$("#subscribe button").css("border", "2px solid #00CD00");
-						$("#subscribe input").css("border", "2px solid #00CD00");
-						$("#subscribe button").html("thanks!");
-					} else {
-						$("#subscribe button").css("background-color", "red");
-						$("#subscribe button").css("border", "2px solid red");
-						$("#subscribe input").css("border", "2px solid red");
-						$("#subscribe button").html("error... try again?");
-					}
+		$(document).ready(function(){
+		/* plagiarized from https://stackoverflow.com/questions/2855865/validating-email-addresses-using-jquery-and-regex, sure hope
+        they know what this means because I sure don't */
+        function isValidEmailAddress(emailAddress) {
+          var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+          return pattern.test(emailAddress);
+        };
+		var formSubmitted = function(status) {
+			if (status === 0) {
+           		var success = "<p> Thanks! </p>";
+				$("#subscribe button").replaceWith(success);
+				$("#subscribe input").remove();
+			} else if (status === 1) {
+				$("#subscribe button").css("background-color", "red");
+				$("#subscribe button").css("border", "2px solid red");
+				$("#subscribe input").css("border", "2px solid red");
+				$("#subscribe button").html("error... try again?");
+			} else {
+            $("#subscribe button").css("background-color", "red");
+            $("#subscribe button").css("border", "2px solid red");
+            $("#subscribe input").css("border", "2px solid red");
+            $("#subscribe button").html("invalid email!");
+          }
 				};
 				$("#subscribe button").click(function(){
 						var email = $("#subscribe input").val();
+            if (!isValidEmailAddress(email)) {
+              formSubmitted(2);
+              return;
+            }
 						$.ajax({
 								type: 'POST',
 								data: {subscriberEmail: email},
 								success: function() {
-									formSubmitted(true);
+									formSubmitted(0);
 								},
 								error: function() {
-									formSubmitted(false);
+									formSubmitted(1);
 								}
 						});
 				});
+
+#subscribe p {
+	color: #00CD00;
+	font-family: 'Roboto Mono', monospace;
+	font-size: 2em;
+	position: relative;
+	z-index: 10;
+}
 
 				$(".loader").fadeOut(1000, "linear");
 
