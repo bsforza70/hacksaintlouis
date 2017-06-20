47b3f85e19aaf31b2ec8b4268572bff0d854c052
Michael Gira <contact@michaelgira.me>
2017-06-20 00:24:52 -0500

    Fix iOS Problems

--- index.php
+++ index.php
@@ -110,163 +110,161 @@
 	<body>
 		<div class="popup-message"></div>
 		<div class="loader"></div>
-		<div id="massContainer">
-			<div class="panel" id="first">
-				<div id="header">
-					<img src="res/Logo.svg">
-					<h1 id="title"> Hack <br> Saint Louis </h1>
-				</div>
-				<div id="subtitle"> Oct. 13-15, 2017 / Some Fancy Venue </div>
-				<form autocomplete="off" id="subscribe">
-					<!-- note: these actually need to be on the same line -->
-					<input type="text" placeholder="email@example.com"><button type="button">get updates</button>
-				</form>
-				<div class="scroll"></div>
+		<div class="panel" id="first">
+			<div id="header">
+				<img src="res/Logo.svg">
+				<h1 id="title"> Hack <br> Saint Louis </h1>
 			</div>
-			<div class="panel doubleddiagonal" id="second">
-				<h1>What is <span id="hsl">Hack Saint Louis</span>?</h1>
-				<p>
-				Hack Saint Louis challenges students to build a hardware or software project in 48 hours, bringing together programmers, designers, leaders and visionaries. Over a weekend of rich learning and intense collaboration, you'll gain hands-on work experience and forge valuable connections with big-name companies, and you'll strengthen friendships and make lasting memories while you're at it. Whether you're a seasoned developer looking to hone your skills or a curious stranger to the computer science world, this is the place for you!
-				</p>
-			</div>
-			<div class="panel doubleddiagonal" id="third">
-				<h1 id="FAQ">Frequently Asked Questions</h1>
-				<ul class="FAQContainer">
-					<li>
-						<div class="Q"> <span> Who can participate? </span> </div>
-						<div class="A"> <span>Any student! If you attend a university, high school, or middle school, you're eligible to hack.</span> </div>
+			<div id="subtitle"> Oct. 13-15, 2017 / Some Fancy Venue </div>
+			<form autocomplete="off" id="subscribe">
+				<!-- note: these actually need to be on the same line -->
+				<input type="text" placeholder="email@example.com"><button type="button">get updates</button>
+			</form>
+			<div class="scroll"></div>
+		</div>
+		<div class="panel doubleddiagonal" id="second">
+			<h1>What is <span id="hsl">Hack Saint Louis</span>?</h1>
+			<p>
+			Hack Saint Louis challenges students to build a hardware or software project in 48 hours, bringing together programmers, designers, leaders and visionaries. Over a weekend of rich learning and intense collaboration, you'll gain hands-on work experience and forge valuable connections with big-name companies, and you'll strengthen friendships and make lasting memories while you're at it. Whether you're a seasoned developer looking to hone your skills or a curious stranger to the computer science world, this is the place for you!
+			</p>
+		</div>
+		<div class="panel doubleddiagonal" id="third">
+			<h1 id="FAQ">Frequently Asked Questions</h1>
+			<ul class="FAQContainer">
+				<li>
+					<div class="Q"> <span> Who can participate? </span> </div>
+					<div class="A"> <span>Any student! If you attend a university, high school, or middle school, you're eligible to hack.</span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> I'm new to programming, can I still come? </span> </div>
+					<div class="A"> <span> Yes! Hack Saint Louis is a place to learn, experiment, and have fun. We'll also have mentors at the event who can answer questions and give crash courses. Remember, everyone starts somewhere.</span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> What should I bring? </span> </div>
+					<div class="A"> <span> Bring a computer, a valid student ID, and a great attitude. Everything else (food, swag, and love) will be provided. </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> How much does Hack Saint Louis cost? </span> </div>
+					<div class="A"> <span> It's entirely free! </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> When and where is Hack Saint Louis? </span> </div>
+					<div class="A"> <span> The event will be held at the beautiful ___. Check in starts at 5:00pm on Oct. 13 and we'll close up shop around 3:00pm on Oct. 15.</span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Do you offer travel reimbursement? </span> </div>
+					<div class="A"> <span> Unfortunately, we cannot guarantee travel reimbursement at this time. We'll update this page if that changes.</span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Do I need a team? </span> </div>
+					<div class="A"> <span> You can come with friends, colleagues, or come alone. If you don't have a team, we'll find you one.</span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Where can I spend the night? </span> </div>
+					<div class="A"> <span> You're free to come and go as you wish, so you can go home to sleep or book a hotel if you're not from the area. However, many people bring sleeping bags and spend the night in the venue. </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Will food be provided? </span> </div>
+					<div class="A"> <span> All meals will be provided, including snacks. </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Will there be prizes? </span> </div>
+					<div class="A"> <span> Yes! There will be cash prizes for the winners. <br> [more details later] </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> How are winners determined? </span> </div>
+					<div class="A"> <span> Teams will present their projects on Sunday, the final day. From these presentations, a panel of judges will select the winners. </span> </div>
+				</li>
+				<li>
+					<div class="Q"> <span> Still have questions? </span> </div>
+					<div class="A"> <span> Feel free to shoot us a message at <a href="mailto:info@hacksaintlouis.com">info@hacksaintlouis.com</a>. We're more than happy to answer questions. </span> </div>
+				</li>
+			</ul>
+		</div>
+		<!-- <svg id="curveUpColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+			<path d="M0 100 C 20 0 50 0 100 100 Z"/>
+		</svg> -->
+		<div id="transitionTwo"> </div>
+		<div id="transitionOne"> </div>
+		<div class="panel" id="fourth">
+			<h1 id="FSchedule">Schedule</h1>
+			<div id="scheduleContainer">
+				<ul id="friday" class="scheduleListContainer">
+					<li class="scheduleListDay scheduleListItem">
+						<span class="scheduleListDayItem"> Friday </span>
 					</li>
-					<li>
-						<div class="Q"> <span> I'm new to programming, can I still come? </span> </div>
-						<div class="A"> <span> Yes! Hack Saint Louis is a place to learn, experiment, and have fun. We'll also have mentors at the event who can answer questions and give crash courses. Remember, everyone starts somewhere.</span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 11:00am </span> <span class="thing"> Check in </span>
 					</li>
-					<li>
-						<div class="Q"> <span> What should I bring? </span> </div>
-						<div class="A"> <span> Bring a computer, a valid student ID, and a great attitude. Everything else (food, swag, and love) will be provided. </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 11:45am </span> <span class="thing"> Opening ceremony </span>
 					</li>
-					<li>
-						<div class="Q"> <span> How much does Hack Saint Louis cost? </span> </div>
-						<div class="A"> <span> It's entirely free! </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 12:15pm </span> <span class="thing"> Hacking begins, lunch </span>
 					</li>
-					<li>
-						<div class="Q"> <span> When and where is Hack Saint Louis? </span> </div>
-						<div class="A"> <span> The event will be held at the beautiful ___. Check in starts at 5:00pm on Oct. 13 and we'll close up shop around 3:00pm on Oct. 15.</span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 2:30pm </span> <span class="thing"> Workshop </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Do you offer travel reimbursement? </span> </div>
-						<div class="A"> <span> Unfortunately, we cannot guarantee travel reimbursement at this time. We'll update this page if that changes.</span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 5:45pm </span> <span class="thing"> Dinner </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Do I need a team? </span> </div>
-						<div class="A"> <span> You can come with friends, colleagues, or come alone. If you don't have a team, we'll find you one.</span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 7:15pm </span> <span class="thing"> Workshop </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Where can I spend the night? </span> </div>
-						<div class="A"> <span> You're free to come and go as you wish, so you can go home to sleep or book a hotel if you're not from the area. However, many people bring sleeping bags and spend the night in the venue. </span> </div>
+				</ul>
+				<ul id="saturday" class="scheduleListContainer">
+					<li class="scheduleListDay scheduleListItem">
+						<span class="scheduleListDayItem"> Saturday </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Will food be provided? </span> </div>
-						<div class="A"> <span> All meals will be provided, including snacks. </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 12:00am </span> <span class="thing"> Midnight snack </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Will there be prizes? </span> </div>
-						<div class="A"> <span> Yes! There will be cash prizes for the winners. <br> [more details later] </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 7:00am </span> <span class="thing"> Breakfast </span>
 					</li>
-					<li>
-						<div class="Q"> <span> How are winners determined? </span> </div>
-						<div class="A"> <span> Teams will present their projects on Sunday, the final day. From these presentations, a panel of judges will select the winners. </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 8:30am </span> <span class="thing"> Workshop </span>
 					</li>
-					<li>
-						<div class="Q"> <span> Still have questions? </span> </div>
-						<div class="A"> <span> Feel free to shoot us a message at <a href="mailto:info@hacksaintlouis.com">info@hacksaintlouis.com</a>. We're more than happy to answer questions. </span> </div>
+					<li class="scheduleListItem">
+						<span class="time"> 12:15pm </span> <span class="thing"> Lunch </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 2:30pm </span> <span class="thing"> Workshop </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 5:45pm </span> <span class="thing"> Dinner </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 7:15pm </span> <span class="thing"> Workshop </span>
+					</li>
+				</ul>
+				<ul id="sunday" class="scheduleListContainer">
+					<li class="scheduleListDay scheduleListItem">
+						<span class="scheduleListDayItem"> Sunday </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 12:00am </span> <span class="thing"> Midnight snack </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 7:00am </span> <span class="thing"> Breakfast </span>
+					</li>
+					<!-- no workshop, just 5 hours of last-minute hacking -->
+					<li class="scheduleListItem">
+						<span class="time"> 12:15pm </span> <span class="thing"> Hacking closes </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 12:30pm </span> <span class="thing"> Judging begins </span>
+					</li>
+					<li class="scheduleListItem">
+						<span class="time"> 1:30pm </span> <span class="thing"> Closing ceremony </span>
 					</li>
 				</ul>
 			</div>
-			<!-- <svg id="curveUpColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
-				<path d="M0 100 C 20 0 50 0 100 100 Z"/>
-			</svg> -->
-			<div id="transitionTwo"> </div>
-			<div id="transitionOne"> </div>
-			<div class="panel" id="fourth">
-				<h1 id="FSchedule">Schedule</h1>
-				<div id="scheduleContainer">
-					<ul id="friday" class="scheduleListContainer">
-						<li class="scheduleListDay scheduleListItem">
-							<span class="scheduleListDayItem"> Friday </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 11:00am </span> <span class="thing"> Check in </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 11:45am </span> <span class="thing"> Opening ceremony </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 12:15pm </span> <span class="thing"> Hacking begins, lunch </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 2:30pm </span> <span class="thing"> Workshop </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 5:45pm </span> <span class="thing"> Dinner </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 7:15pm </span> <span class="thing"> Workshop </span>
-						</li>
-					</ul>
-					<ul id="saturday" class="scheduleListContainer">
-						<li class="scheduleListDay scheduleListItem">
-							<span class="scheduleListDayItem"> Saturday </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 12:00am </span> <span class="thing"> Midnight snack </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 7:00am </span> <span class="thing"> Breakfast </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 8:30am </span> <span class="thing"> Workshop </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 12:15pm </span> <span class="thing"> Lunch </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 2:30pm </span> <span class="thing"> Workshop </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 5:45pm </span> <span class="thing"> Dinner </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 7:15pm </span> <span class="thing"> Workshop </span>
-						</li>
-					</ul>
-					<ul id="sunday" class="scheduleListContainer">
-						<li class="scheduleListDay scheduleListItem">
-							<span class="scheduleListDayItem"> Sunday </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 12:00am </span> <span class="thing"> Midnight snack </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 7:00am </span> <span class="thing"> Breakfast </span>
-						</li>
-						<!-- no workshop, just 5 hours of last-minute hacking -->
-						<li class="scheduleListItem">
-							<span class="time"> 12:15pm </span> <span class="thing"> Hacking closes </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 12:30pm </span> <span class="thing"> Judging begins </span>
-						</li>
-						<li class="scheduleListItem">
-							<span class="time"> 1:30pm </span> <span class="thing"> Closing ceremony </span>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div id="transitionOne"> </div>
-			<div id="transitionTwo"> </div>
-			<div class="panel" id="fifth">
-				<h1 id="TSponsors">Sponsors</h1>
-			</div>
+		</div>
+		<div id="transitionOne"> </div>
+		<div id="transitionTwo"> </div>
+		<div class="panel" id="fifth">
+			<h1 id="TSponsors">Sponsors</h1>
 		</div>
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
